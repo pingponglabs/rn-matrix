b8d52952a52102824f39a781ffcbdbcc9f2e3616
Annie Elequin <annie@elequin.io>
2020-08-20 00:48:46 -0500

    update npm ignore

--- RnMatrixCore/index.js
+++ RnMatrixCore/index.js
@@ -1,14 +1,13 @@
 /**
  * @format
  */
-import './shim.js';
+// import './shim.js';
 
-import {AppRegistry} from 'react-native';
-import {name as appName} from './app.json';
-import App from './App';
+// import {AppRegistry} from 'react-native';
+// import {name as appName} from './app.json';
+// import App from './App';
+// AppRegistry.registerComponent(appName, () => App);
+
 import matrix from './src/services/external';
 import matrixSdk from 'matrix-js-sdk';
-
 export {matrix, matrixSdk};
-
-AppRegistry.registerComponent(appName, () => App);
