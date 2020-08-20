246f72a7008c6dbeddcf43325a52b3879b615769
Annie Elequin <annie@elequin.io>
2020-08-20 01:22:38 -0500

    updated example and tested it out

--- RnMatrixExample/index.js
+++ RnMatrixExample/index.js
@@ -1,9 +1,6 @@
-/**
- * @format
- */
+import './shim.js';
 
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
-
+import App from './App';
 AppRegistry.registerComponent(appName, () => App);
